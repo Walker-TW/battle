62137a5b6536bbda91f7d3c0b3a95cda7d6b008b
Walker-TW <thomas.w.walker@live.co.uk>
2020-01-08 21:56:23 +0000

    Test - Testing Infastructure & README.md added'

---
+++ README.md
@@ -0,0 +1,3 @@
+An app to simulate an online game in which two competing "players" fight and thier HP is removed based on attack power.
+
+Built using Ruby, Sinatra and Capybara (for testing).
---
+++ spec/features/testing_infastructure_spec.rb
@@ -0,0 +1,12 @@
+
+feature 'Testing infrastructure' do
+  scenario 'Can run app and check page content' do
+    visit('/')
+    expect(page).to have_content 'Lets test if this works'
+  end
+
+  scenario 'Testing if the form works' do
+    visit('/butts')
+    expect(page).to have_content 'butts butts butts'
+  end
+end
